8a3071602e31a10665338c50d8cce25b82ad5046
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-09 22:11:27 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -5,5 +5,5 @@ puts "supercalifragilisticexpialidocious".length
 puts big_word="supercalifragilisticexpialidocious"
 puts bigger_word=big_word+"mom"+"wow"
 puts "i really like programming".capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub"A","O"
 your_favorite_word = "sentient"
