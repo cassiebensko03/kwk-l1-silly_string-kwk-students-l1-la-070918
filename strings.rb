11a49c378731378c37453723985183eeb729f20f
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-09 22:04:43 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -2,7 +2,7 @@ puts "sentient"
 puts "sentient".upcase
 puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
-puts big_word="supercalifragilisticexpialidocious""wow" + "mom"
+big_word="supercalifragilisticexpialidocious"
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 your_favorite_word = "sentient"
