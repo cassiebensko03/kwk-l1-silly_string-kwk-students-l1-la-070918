c3c091073a4609bab28805bd14d00342aed1eb29
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-09 22:09:32 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -2,7 +2,7 @@ puts "sentient"
 puts "sentient".upcase
 puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
-big_word="supercalifragilisticexpialidocious"
+puts big_word="supercalifragilisticexpialidocious"
 bigger_word=big_word+"mom"+"wow"
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
