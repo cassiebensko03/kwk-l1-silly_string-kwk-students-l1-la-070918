b67446ae8711830565f0ae5c85231b1ae2add4de
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-09 22:12:38 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -6,4 +6,4 @@ puts big_word="supercalifragilisticexpialidocious"
 puts bigger_word=big_word+"mom"+"wow"
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub"A","O"
-your_favorite_word = "sentient"
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub"a","o"
