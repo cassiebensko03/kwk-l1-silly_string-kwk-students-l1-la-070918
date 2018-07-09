cb3cf7341aa92f06c999b724200733305584e9e4
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-09 21:59:55 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -5,5 +5,4 @@ puts "supercalifragilisticexpialidocious".length
 puts "wow" + "mom"
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".
-
 your_favorite_word = "sentient"
